696f3452216f2fc53c36e8ef68a98f8e12da5e57
Sally Perez <seattlesally1@gmail.com>
2021-03-12 16:56:19 -0800

    installed react rotuer and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/Navbar"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>My Google Books</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
